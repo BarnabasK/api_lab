46708b0cfb8fb6c545877f8567bd1a631b99fc94
= <=>
2016-11-01 18:18:07 +0300

    Changing from urllib to urllib2 for Python 2.7

--- api.py
+++ api.py
@@ -1,15 +1,16 @@
-import urllib
-import urllib.parse
-import urllib.request
+from urllib2 import urlopen
+from urllib2 import Request
+from urlparse import urlparse
 import pprint
 import json
 import sys
 
 def geocode(address_str):
-    address = urllib.parse.quote(address_str)
-    geocode_url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" % address
-    req = urllib.request.urlopen(geocode_url)
-    jsonResponse = json.loads(req.read().decode('utf-8'))
+    api_key = 'AIzaSyAfF5CgQO37zohUQI0CgaidJQ65YeYus6I' 
+    geocode_url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=true'+ address_str + api_key
+    req = Request(geocode_url)
+    res = urlopen(req)
+    jsonResponse = json.loads(res.read().decode('utf-8'))
 
     status_code = jsonResponse['status']
     if status_code != 'OK':
@@ -18,4 +19,8 @@ def geocode(address_str):
         lat = jsonResponse['results'][0]['geometry']['location']['lat']
         lng = jsonResponse['results'][0]['geometry']['location']['lng']
         geo_location= {"Latitude:": lat, "Longitude:": lng}
-    return geo_location
+        print geo_location
+
+geocode("Nakuru")
+
+
